f224b646c3e691816fe5168ebf4fd6a3ddd62131
Ajay Chambers <W3Dojo@Gmail.com>
2026-06-06 01:23:40 -0700

    Reconfigured CMakeGlobals.cmake to be less j-tool specific.

--- CMakeGlobals.cmake
+++ CMakeGlobals.cmake
@@ -1,70 +1,106 @@
-list(PREPEND CMAKE_MODULE_PATH "${CMAKE_CURRENT_SOURCE_DIR}/cmake/scripts")
-include("${CMAKE_SOURCE_DIR}/cmake/scripts/cache-var.cmake")
-#
-#
+list(PREPEND CMAKE_MODULE_PATH
+    "${CMAKE_SOURCE_DIR}/cmake/scripts"
+    "${CMAKE_SOURCE_DIR}/cmake/scripts/cache-var-mod")
+
+include("${CMAKE_SOURCE_DIR}/cmake/scripts/cache-var-mod/cache-var.cmake")
+
+
+
+
 #############################################
 # CACHED STRING VARIABLES
 #############################################
 cache_var(
-  JT_DESCRIPTION
-  "J-Tool is a set of tools for working with, and manipulating, JSON"
-  "J-Tool's description"
+  PROJ_NAME_
+  "J-TOOL"
+  "The name of the project."
   FORCE)
 
+
 cache_var(
-  JT_VERSION
-  "0.1.0"
-  "J-Tool's version number"
+  PROJ_DESC_
+  "J-Tool is a set of tools for working with, and manipulating, JSON"
+  "The description of the project."
   FORCE)
-#
-#
+
+
+cache_var(
+  PROJ_VER_
+  "0.1.0"
+  "The version number of the project."
+  FORCE)
+
+
+
+
 #############################################
 # CACHED DIRECTORY PATHS
 #############################################
 cache_path(
-  JT_ROOTDIR
+  ROOT_DIR_
   "${CMAKE_SOURCE_DIR}"
-  "J-Tool project's root directory"
+  "Project root directory (contains the main CMakeLists.txt file)."
   FORCE)
 
-cache_path(
-  JT_INCLUDE_DIR
-  "${JT_ROOTDIR}/src/include"
-  "J-Tool project's include directory"
-  FORCE)
 
 cache_path(
-  JT_TEST_DIR
-  "${JT_ROOTDIR}/test"
-  "Path to J-Tool unit-test source code"
+  INCLUDE_DIR_
+  "${CMAKE_SOURCE_DIR}/include"
+  "Project include directory where public headers (.hxx) are stored."
   FORCE)
 
+
 cache_path(
-  JT_BUILD_DIR
+  SOURCE_DIR_
+  "${CMAKE_SOURCE_DIR}/src"
+  "Project source directory."
+  FORCE)
+
+
+cache_path(
+  BUILD_DIR_
+  "${CMAKE_BINARY_DIR}"
+  "Project build directory."
+  FORCE)
+
+
+cache_path(
+  RUNTIME_DIR_
   "${CMAKE_BINARY_DIR}/bin"
-  "J-Tool project's build directory"
+  "The location where executable binaries are built & stored."
   FORCE)
 
-cache_path(
-  JT_BIN_DIR
-  "${CMAKE_BINARY_DIR}/bin"
-  "Directory where J-Tool executable binaries are built & stored"
-  FORCE)
 
 cache_path(
-  JT_TEST_BIN_DIR
-  "${CMAKE_BINARY_DIR}/test/bin"
-  "Directory where J-Tool's unit-tests are built & stored"
-  FORCE)
-
-cache_path(
-  JT_LIB_DIR
+  ARCHIVE_DIR_
   "${CMAKE_BINARY_DIR}/lib"
-  "Directory where J-Tool's libraries are built & stored"
+  "The location where static libs are built & stored."
   FORCE)
 
+
 cache_path(
-  JT_CMAKE_MODULES
-  "${JT_ROOTDIR}/cmake"
-  "Directory where J-Tool's CMake modules are stored"
+  LIBRARY_DIR_
+  "${CMAKE_BINARY_DIR}/lib"
+  "The location where dynamic libs (.dll, .so, .dylib) are built & stored."
+  FORCE)
+
+
+cache_path(
+  TEST_DIR_
+  "${CMAKE_SOURCE_DIR}/test"
+  "Project test directory where unit-test source files are stored."
+  FORCE)
+
+
+cache_path(
+  TEST_RUNTIME_DIR_
+  "${CMAKE_BINARY_DIR}/test/bin"
+  "Location where unit-test executable binaries are built & stored."
+  FORCE)
+
+
+cache_path(
+  MODULE_DIR_
+  "${CMAKE_SOURCE_DIR}/cmake/scripts"
+  "Project CMake module directory where custom CMake scripts are stored."
   FORCE)
